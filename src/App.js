e2947dacbf90560b9e92aab55a012d3d2d54a3f2
Aniket <aniket.v.tarapurkar99@gmail.com>
2023-04-21 17:32:04 +0530

    adding few comments to our app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./home";
 import "./index.css";
 
+//APP COMPONENT
 function App() {
   return (
     <div className="app">
--- src/home.js
+++ src/home.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./index.css";
 
+//HOME COMPONENT
 function Home() {
+  //USEING HOOKS USESTATE HOOK AND INITIALIZING FOR  ACTIVITY AND LIST
   const [activity, setActivity] = useState("");
   const [listdata, setlistData] = useState([]);
+
+  // FUNCTIONALITY FOR ADDACTIVITY
   function addActivity() {
-    // setlistData([...listdata, activity]);            // another way to render a function this
-    // console.log(listdata);
     setlistData((listdata) => {
       const updatedList = [...listdata, activity];
       console.log(updatedList);
@@ -14,17 +16,20 @@ function Home() {
       return updatedList;
     });
   }
-
+  // FUNCTIONALITY FOR REMOVEACTIVITY FROM LIST DATA
   function removeActivity(i) {
     const updatedlistdata = listdata.filter((elem, id) => {
       return i !== id;
     });
     setlistData(updatedlistdata);
   }
+
+  // FUNCTIONALITY FOR ALLREMOVEACTIVITY FROM LIST DATA
   function removeAll() {
     setlistData([]);
   }
   return (
+    //IMPLEMENTING SOME UI
     <div className="container">
       <div className="header">Todo List</div>
       <input
@@ -35,7 +40,7 @@ function Home() {
           setActivity(e.target.value);
         }}
       />
-      <button onClick={addActivity}>Add Activity </button>
+      <button onClick={`listdata.length==0?)`}>Add Activity </button>
 
       <p className="list-heading">Here is your List :{")"}</p>
       {listdata !== [] &&
@@ -43,7 +48,9 @@ function Home() {
           return (
             <>
               <p key={i}>
-                <div className="listData ">{data}</div>
+                <div className="listData ">
+                  {data.data > 0 ? "plz enter something" : data}
+                </div>
 
                 <div className="btn-position ">
                   <button onClick={() => removeActivity(i)}>Remove(-)</button>
